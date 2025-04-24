bc95a16b4120d5f3da9e5a262d5bbe3862f71fd1
Dhaval Solanki <dhavalas24@gmail.com>
2025-04-24 16:42:29 +0530

    `Refactor navbar and mobile menu functionality`

--- gym.js
+++ gym.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const navLinks = document.querySelectorAll('nav ul > li > a');
+	const checkBox = document.getElementById('check');
   
 	navLinks.forEach(link => {
 	  link.addEventListener('click', function () {
+		// Remove active from all and set the current one
 		navLinks.forEach(nav => nav.classList.remove('active'));
 		this.classList.add('active');
+  
+		// Hide the nav menu on mobile
+		if (checkBox.checked) {
+		  checkBox.checked = false;
+		}
 	  });
 	});
   });
