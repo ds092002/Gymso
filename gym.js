8d9490a73ba7a57d6eaff498bece260055555b09
Dhaval Solanki <dhavalas24@gmail.com>
2025-04-25 10:46:23 +0530

    `Added event listener to checkbox and updated nav link click event to uncheck checkbox on mobile`

--- gym.js
+++ gym.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const navLinks = document.querySelectorAll('nav ul > li > a');
-  
+	const checkbox = document.getElementById('check');
+
 	navLinks.forEach(link => {
-	  link.addEventListener('click', function () {
-		navLinks.forEach(nav => nav.classList.remove('active'));
-		this.classList.add('active');
-	  });
+		link.addEventListener('click', function () {
+			// Remove 'active' class from all and add to current
+			navLinks.forEach(nav => nav.classList.remove('active'));
+			this.classList.add('active');
+
+			// Uncheck the checkbox to close the navbar on mobile
+			if (checkbox.checked) {
+				checkbox.checked = false;
+			}
+		});
 	});
-  });
+});
